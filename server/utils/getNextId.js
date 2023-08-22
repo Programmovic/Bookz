cbfc88840b3ac1f6ace9acda81d514553bcd9c3d
Adam <adamsoliman800@gmail.com>
2023-08-22 17:14:15 +0300

    update getNextId.js

--- server/utils/getNextId.js
+++ server/utils/getNextId.js
@@ -4,8 +4,5 @@ const User = require('../models/User');
 // =============================================================================
 module.exports = async function getNextId() {
     const lastUser = await User.findOne().sort({ id: -1 });
-    if (!lastUser) {
-        return 1;
-    }
-    return lastUser.id + 1;
+    return lastUser ? lastUser.id + 1 : 1;
 }
