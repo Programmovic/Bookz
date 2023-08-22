39865f5f3969088de665583629dce92ce2cd7cf3
Adam <adamsoliman800@gmail.com>
2023-08-22 17:05:52 +0300

    update getNextId.js

--- server/utils/getNextId.js
+++ server/utils/getNextId.js
@@ -2,7 +2,7 @@ const User = require('../models/User');
 // =============================================================================
 // // Helper function to get the next ID for a User
 // =============================================================================
-module.exports = async function getNextStageId() {
+module.exports = async function getNextId() {
     const lastUser = await User.findOne().sort({ id: -1 });
     if (!lastUser) {
         return 1;
